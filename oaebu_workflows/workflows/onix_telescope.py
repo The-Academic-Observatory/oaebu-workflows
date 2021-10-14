c81918929ce00430cfaac493d5f84e3a3f416eed
Jamie Diprose <jamie.diprose@gmail.com>
2021-10-15 11:46:18 +1300

    Update onix parser

--- oaebu_workflows/workflows/onix_telescope.py
+++ oaebu_workflows/workflows/onix_telescope.py
@@ -49,7 +49,7 @@ class OnixRelease(SnapshotRelease):
     DOWNLOAD_FILES_REGEX = r"^.*\.(onx|xml)$"
     TRANSFORM_FILES_REGEX = "onix.jsonl"
     ONIX_PARSER_NAME = "coki-onix-parser.jar"
-    ONIX_PARSER_URL = "https://github.com/The-Academic-Observatory/onix-parser/releases/download/v1.1/coki-onix-parser-1.2-SNAPSHOT-shaded.jar"
+    ONIX_PARSER_URL = "https://github.com/The-Academic-Observatory/onix-parser/releases/download/v1.2/coki-onix-parser-1.2-SNAPSHOT-shaded.jar"
     ONIX_PARSER_MD5 = "bf223124df9e93cdb1cbb5d7dc71080c"
 
     def __init__(
