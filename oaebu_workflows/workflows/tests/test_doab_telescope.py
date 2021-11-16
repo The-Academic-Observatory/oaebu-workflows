5b8efc7f886d648353ca2cedba5cba8ed16d4f6f
Jamie Diprose <jamie.diprose@gmail.com>
2021-11-16 13:49:33 +1300

    Test with a set

--- oaebu_workflows/workflows/tests/test_doab_telescope.py
+++ oaebu_workflows/workflows/tests/test_doab_telescope.py
@@ -241,7 +241,7 @@ class TestDoabTelescope(ObservatoryTestCase):
         """Cover case when airflow_vars is given."""
 
         telescope = DoabTelescope(airflow_vars=[AirflowVars.DOWNLOAD_BUCKET])
-        self.assertEqual(telescope.airflow_vars, [AirflowVars.DOWNLOAD_BUCKET, AirflowVars.TRANSFORM_BUCKET])
+        self.assertEqual(set(telescope.airflow_vars), {AirflowVars.DOWNLOAD_BUCKET, AirflowVars.TRANSFORM_BUCKET})
 
     @patch("observatory.platform.utils.workflow_utils.Variable.get")
     def test_download(self, mock_variable_get):
