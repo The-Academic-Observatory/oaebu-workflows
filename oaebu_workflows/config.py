e581cce7a6086f941d03f3123e2cf2367ad43bb2
Jamie Diprose <5715104+jdddog@users.noreply.github.com>
2023-08-10 14:26:46 +1200

    Remove related functions

--- oaebu_workflows/config.py
+++ oaebu_workflows/config.py
@@ -14,15 +14,9 @@
 
 # Author: James Diprose
 
-import json
 import os
-import re
-from typing import Dict
-
-from airflow.exceptions import AirflowException
 
 from observatory.platform.config import module_file_path
-from observatory.platform.utils.jinja2_utils import render_template
 
 
 def test_fixtures_folder(*subdirs) -> str:
@@ -51,48 +45,3 @@ def sql_folder() -> str:
     """
 
     return module_file_path("oaebu_workflows.database.sql")
-
-
-####################################
-### For Elastic workflow Imports ###
-####################################
-
-
-def load_elastic_mappings_oaebu(path: str, table_prefix: str) -> Dict:
-    """For the OAEBU project, load the Elastic mappings for a given table_prefix.
-    :param path: the path to the mappings files.
-    :param table_prefix: the table_id prefix (without shard date).
-    :return: the rendered mapping as a Dict.
-    """
-
-    if not table_prefix.startswith("oaebu"):
-        raise ValueError("Table must begin with 'oaebu'")
-    elif "unmatched" in table_prefix:
-        mappings_path = os.path.join(path, "oaebu-unmatched-metrics-mappings.json.jinja2")
-        return json.loads(render_template(mappings_path))
-    elif "institution_list" in table_prefix:
-        mappings_path = os.path.join(path, "oaebu-institution-list-mappings.json.jinja2")
-        return json.loads(render_template(mappings_path))
-    else:
-        # Aggregation level
-        aggregation_level_search = re.search(r"(?<=book_)(.*?)(?=_)", table_prefix)
-        if aggregation_level_search:
-            aggregation_level = aggregation_level_search.group(1)
-        else:
-            raise AirflowException(f"Aggregation Level not found in table_prefix: {table_prefix}")
-
-        # Make mappings path
-        suffix = re.search(f"_book_{aggregation_level}_(.*)", table_prefix).group(1)
-        mappings_file_name = f"oaebu-{suffix}-mappings.json.jinja2".replace("_", "-")
-        mappings_path = os.path.join(path, mappings_file_name)
-
-        return json.loads(render_template(mappings_path, aggregation_level=aggregation_level))
-
-
-def elastic_mappings_folder() -> str:
-    """Get the Elasticsearch mappings path.
-
-    :return: the elastic search schema path.
-    """
-
-    return module_file_path("oaebu_workflows.database.mappings")
