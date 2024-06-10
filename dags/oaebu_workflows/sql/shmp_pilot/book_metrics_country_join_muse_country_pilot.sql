e91dd939d9211bf05a38293338665032a30e8827
keegansmith21 <keegan.r.s21@gmail.com>
2024-06-10 06:33:36 +0000

    Fix muse country metrics

--- dags/oaebu_workflows/sql/shmp_pilot/book_metrics_country_join_muse_country_pilot.sql
+++ dags/oaebu_workflows/sql/shmp_pilot/book_metrics_country_join_muse_country_pilot.sql
@@ -1,4 +1,3 @@
--- for muse collections, the alpha2 value is the full country name and needs to be matched on country_muse_name 
 LEFT JOIN muse_month_country as muse_country_pilot ON month_country.ISBN13 = muse_country_pilot.ISBN13
 AND month_country.month = muse_country_pilot.month
-AND month_country.alpha2 = muse_country_pilot.alpha2
+AND (month_country.country_iso_name = muse_country_pilot.country_name OR month_country.country_wikipedia_name = muse_country_pilot.country_name)
