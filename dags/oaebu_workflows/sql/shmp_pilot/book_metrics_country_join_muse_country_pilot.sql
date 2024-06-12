e06ae9e671b9d3acf2c17454fd79037b2b620842
keeagnsmith21 <keegan.r.s21@gmail.com>
2024-06-12 10:16:27 +0800

    Added muse country

--- dags/oaebu_workflows/sql/shmp_pilot/book_metrics_country_join_muse_country_pilot.sql
+++ dags/oaebu_workflows/sql/shmp_pilot/book_metrics_country_join_muse_country_pilot.sql
@@ -1,3 +1,3 @@
 LEFT JOIN muse_month_country as muse_country_pilot ON month_country.ISBN13 = muse_country_pilot.ISBN13
 AND month_country.month = muse_country_pilot.month
-AND (month_country.country_iso_name = muse_country_pilot.country_name OR month_country.country_wikipedia_name = muse_country_pilot.country_name)
+AND month_country.country_muse_name = muse_country_pilot.country_name
