86b1226fa5cf80abbbc4f6637d1736c3fbbdb09c
keeagnsmith21 <keegan.r.s21@gmail.com>
2026-07-09 09:49:07 +0800

    google_books telescope

--- tests/google_books_telescope/test_google_books_telescope.py
+++ tests/google_books_telescope/test_google_books_telescope.py
@@ -34,10 +34,10 @@ from oaebu_workflows.google_books_telescope.google_books_telescope import (
     gb_transform,
     _gb_early_stop,
 )
-from observatory_platform.airflow.workflow import Workflow
+
+from observatory_platform.airflow.workflow import Workflow, make_workflow_folder
 from observatory_platform.dataset_api import DatasetAPI
 from observatory_platform.google.bigquery import bq_table_id
-from observatory_platform.google.gcs import gcs_blob_name_from_path
 from observatory_platform.sandbox.sandbox_environment import SandboxEnvironment
 from observatory_platform.sandbox.test_utils import SandboxTestCase, find_free_port
 from observatory_platform.sandbox.sftp_server import SftpServer
@@ -138,206 +138,107 @@ class TestGoogleBooksTelescope(SandboxTestCase):
         dataset_id = env.add_dataset()
 
         # Create the Observatory environment and run tests
-        with env.create():
-            with sftp_server.create() as sftp_root:
+        with env.create(), sftp_server.create() as sftp_root:
+            # Setup DAG
+            logical_date = pendulum.datetime(year=2021, month=3, day=31)
+            sales_partner = partner_from_str("google_books_sales")
+            sales_partner.bq_dataset_id = dataset_id
+            traffic_partner = partner_from_str("google_books_traffic")
+            traffic_partner.bq_dataset_id = dataset_id
+            sftp_service_conn_id = "sftp_service"
+            dag_id = "google_books_test"
+            api_bq_dataset_id = env.add_dataset()
+            dag = create_dag(
+                dag_id=dag_id,
+                cloud_workspace=env.cloud_workspace,
+                sftp_root="/",  # Unintuitive, but this is correct
+                sales_partner=sales_partner,
+                traffic_partner=traffic_partner,
+                sftp_service_conn_id=sftp_service_conn_id,
+                api_bq_dataset_id=api_bq_dataset_id,
+                retries=0,
+            )
+            env.serialize_dag(dag)
 
-                # Setup DAG
-                logical_date = pendulum.datetime(year=2021, month=3, day=31)
-                sales_partner = partner_from_str("google_books_sales")
-                sales_partner.bq_dataset_id = dataset_id
-                traffic_partner = partner_from_str("google_books_traffic")
-                traffic_partner.bq_dataset_id = dataset_id
-                sftp_service_conn_id = "sftp_service"
-                dag_id = "google_books_test"
-                api_bq_dataset_id = env.add_dataset()
-                dag = create_dag(
-                    dag_id=dag_id,
-                    cloud_workspace=env.cloud_workspace,
-                    sftp_root="/",  # Unintuitive, but this is correct
-                    sales_partner=sales_partner,
-                    traffic_partner=traffic_partner,
-                    sftp_service_conn_id=sftp_service_conn_id,
-                    api_bq_dataset_id=api_bq_dataset_id,
-                )
+            # Add file to SFTP server
+            sftp_folders = SftpFolders(dag_id, sftp_conn_id=sftp_service_conn_id, sftp_root=sftp_root)
+            os.makedirs(sftp_folders.upload, exist_ok=True)
+            for file_name, file_path in params["test_files"].items():
+                upload_file = os.path.join(sftp_folders.upload, file_name)
+                shutil.copy(file_path, upload_file)
+            # Add SFTP connection
+            env.add_connection(
+                Connection(conn_id=sftp_service_conn_id, uri=f"ssh://:password@localhost:{self.sftp_port}")
+            )
 
-                # Add SFTP connection
-                env.add_connection(
-                    Connection(conn_id=sftp_service_conn_id, uri=f"ssh://:password@localhost:{self.sftp_port}")
-                )
-                with env.create_dag_run(dag, logical_date=logical_date):
-                    # Test that all dependencies are specified: no error should be thrown
-                    ti = env.run_task("check_dependencies")
-                    self.assertEqual(ti.state, State.SUCCESS)
+            now = pendulum.now("UTC")
+            with patch("oaebu_workflows.google_books_telescope.google_books_telescope.pendulum.now") as mock_now:
+                mock_now.return_value = now
+                dagrun = dag.test(logical_date=logical_date)
+            self.assertEqual(dagrun.state, "success")
+            # Test that all dependencies are specified: no error should be thrown
 
-                    # Add file to SFTP server
-                    sftp_folders = SftpFolders(dag_id, sftp_conn_id=sftp_service_conn_id, sftp_root=sftp_root)
-                    os.makedirs(sftp_folders.upload, exist_ok=True)
-                    for file_name, file_path in params["test_files"].items():
-                        upload_file = os.path.join(sftp_folders.upload, file_name)
-                        shutil.copy(file_path, upload_file)
+            # Test that data loaded into BigQuery
+            table_id = bq_table_id(
+                env.cloud_workspace.project_id,
+                sales_partner.bq_dataset_id,
+                sales_partner.bq_table_name,
+            )
+            self.assert_table_integrity(table_id, params["bq_rows"])
+            table_id = bq_table_id(
+                env.cloud_workspace.project_id,
+                traffic_partner.bq_dataset_id,
+                traffic_partner.bq_table_name,
+            )
+            self.assert_table_integrity(table_id, params["bq_rows"])
 
-                    # Test that make release is successful
-                    ti = env.run_task("fetch_releases")
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    release_dicts = ti.xcom_pull(task_ids="fetch_releases", include_prior_dates=False)
-                    expected_release_dicts = _normalise_release_dict(
-                        [
-                            {
-                                "dag_id": "google_books_test",
-                                "run_id": "scheduled__2021-03-31T00:00:00+00:00",
-                                "partition_date": "2020-02-29",
-                                "sftp_files": [
-                                    "/workflows/google_books_test/in_progress/GoogleBooksTrafficReport_2020_02.csv",
-                                    "/workflows/google_books_test/in_progress/GoogleSalesTransactionReport_2020_02.csv",
-                                ],
-                            }
-                        ]
-                    )
-                    # sort sftp_files because we don't care about the order when comparing
-                    self.assertEqual(_normalise_release_dict(release_dicts), expected_release_dicts)
-                    release = GoogleBooksRelease.from_dict(release_dicts[0])
+            release_ti = dagrun.get_task_instance(task_id="fetch_releases")
+            self.assertEqual(release_ti.state, State.SUCCESS)
+            release_dicts = release_ti.xcom_pull(task_ids="fetch_releases", include_prior_dates=False)
+            release = GoogleBooksRelease.from_dict(release_dicts[0])
+            # Set up the API and check
+            api = DatasetAPI(bq_project_id=self.project_id, bq_dataset_id=api_bq_dataset_id)
+            dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_sales")
+            self.assertEqual(len(dataset_releases), 1)
+            expected_release = {
+                "dag_id": dag_id,
+                "entity_id": "google_books_sales",
+                "dag_run_id": release.run_id,
+                "created": now.to_iso8601_string(),
+                "modified": now.to_iso8601_string(),
+                "data_interval_start": "2021-03-31T00:00:00Z",
+                "data_interval_end": "2021-03-31T00:00:00Z",
+                "snapshot_date": None,
+                "partition_date": "2020-02-29T00:00:00Z",
+                "changefile_start_date": None,
+                "changefile_end_date": None,
+                "sequence_start": None,
+                "sequence_end": None,
+                "extra": {},
+            }
+            self.assertEqual(dataset_releases[0].to_dict(), expected_release)
+            dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_traffic")
+            self.assertEqual(len(dataset_releases), 1)
+            expected_release = {
+                "dag_id": dag_id,
+                "entity_id": "google_books_traffic",
+                "dag_run_id": release.run_id,
+                "created": now.to_iso8601_string(),
+                "modified": now.to_iso8601_string(),
+                "data_interval_start": "2021-03-31T00:00:00Z",
+                "data_interval_end": "2021-03-31T00:00:00Z",
+                "snapshot_date": None,
+                "partition_date": "2020-02-29T00:00:00Z",
+                "changefile_start_date": None,
+                "changefile_end_date": None,
+                "sequence_start": None,
+                "sequence_end": None,
+                "extra": {},
+            }
+            self.assertEqual(dataset_releases[0].to_dict(), expected_release)
 
-                    # Test move file to in progress
-                    ti = env.run_task("process_release.move_files_to_in_progress", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    for file in release.sftp_files:
-                        file_name = os.path.basename(file)
-                        upload_file = os.path.join(sftp_folders.upload, file_name)
-                        self.assertFalse(os.path.isfile(upload_file))
-                        in_progress_file = os.path.join(sftp_folders.in_progress, file_name)
-                        self.assertTrue(os.path.isfile(in_progress_file))
-
-                    # Run main telescope tasks
-                    ti = env.run_task("process_release.download", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    ti = env.run_task("process_release.transform", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    ti = env.run_task("process_release.move_files_to_finished", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    ti = env.run_task("process_release.bq_load", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-
-                    # Make assertions for the above tasks
-                    # Test download
-                    self.assertTrue(os.path.exists(release.download_traffic_path))
-                    self.assertTrue(os.path.exists(release.download_sales_path))
-                    self.assert_file_integrity(release.download_traffic_path, params["traffic_download_hash"], "md5")
-                    self.assert_file_integrity(release.download_sales_path, params["sales_download_hash"], "md5")
-
-                    # Test upload downloaded
-                    self.assert_blob_integrity(
-                        env.download_bucket,
-                        gcs_blob_name_from_path(release.download_traffic_path),
-                        release.download_traffic_path,
-                    )
-                    self.assert_blob_integrity(
-                        env.download_bucket,
-                        gcs_blob_name_from_path(release.download_sales_path),
-                        release.download_sales_path,
-                    )
-
-                    # Test that file transformed
-                    self.assertTrue(os.path.exists(release.transform_sales_path))
-                    self.assertTrue(os.path.exists(release.transform_traffic_path))
-                    self.assert_file_integrity(release.transform_sales_path, params["sales_transform_hash"], "gzip_crc")
-                    self.assert_file_integrity(
-                        release.transform_traffic_path, params["traffic_transform_hash"], "gzip_crc"
-                    )
-
-                    # Test that transformed file uploaded
-                    self.assert_blob_integrity(
-                        env.transform_bucket,
-                        gcs_blob_name_from_path(release.transform_traffic_path),
-                        release.transform_traffic_path,
-                    )
-                    self.assert_blob_integrity(
-                        env.transform_bucket,
-                        gcs_blob_name_from_path(release.transform_sales_path),
-                        release.transform_sales_path,
-                    )
-
-                    # Test that files correctly moved to "finished"
-                    for file in release.sftp_files:
-                        file_name = os.path.basename(file)
-                        in_progress_file = os.path.join(sftp_folders.in_progress, file_name)
-                        self.assertFalse(os.path.isfile(in_progress_file))
-
-                        finished_file = os.path.join(sftp_folders.finished, file_name)
-                        self.assertTrue(os.path.isfile(finished_file))
-
-                    # Test that data loaded into BigQuery
-                    table_id = bq_table_id(
-                        env.cloud_workspace.project_id,
-                        sales_partner.bq_dataset_id,
-                        sales_partner.bq_table_name,
-                    )
-                    self.assert_table_integrity(table_id, params["bq_rows"])
-                    table_id = bq_table_id(
-                        env.cloud_workspace.project_id,
-                        traffic_partner.bq_dataset_id,
-                        traffic_partner.bq_table_name,
-                    )
-                    self.assert_table_integrity(table_id, params["bq_rows"])
-
-                    # Set up the API and check
-                    api = DatasetAPI(bq_project_id=self.project_id, bq_dataset_id=api_bq_dataset_id)
-                    dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_sales")
-                    self.assertEqual(len(dataset_releases), 0)
-                    dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_traffic")
-                    self.assertEqual(len(dataset_releases), 0)
-
-                    # Add_dataset_release_task
-                    now = pendulum.now("UTC")
-                    with patch(
-                        "oaebu_workflows.google_books_telescope.google_books_telescope.pendulum.now"
-                    ) as mock_now:
-                        mock_now.return_value = now
-                        ti = env.run_task("process_release.add_new_dataset_release", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_sales")
-                    self.assertEqual(len(dataset_releases), 1)
-                    expected_release = {
-                        "dag_id": dag_id,
-                        "entity_id": "google_books_sales",
-                        "dag_run_id": release.run_id,
-                        "created": now.to_iso8601_string(),
-                        "modified": now.to_iso8601_string(),
-                        "data_interval_start": "2021-03-31T00:00:00Z",
-                        "data_interval_end": "2021-03-31T12:00:00Z",
-                        "snapshot_date": None,
-                        "partition_date": "2020-02-29T00:00:00Z",
-                        "changefile_start_date": None,
-                        "changefile_end_date": None,
-                        "sequence_start": None,
-                        "sequence_end": None,
-                        "extra": {},
-                    }
-                    self.assertEqual(dataset_releases[0].to_dict(), expected_release)
-                    dataset_releases = api.get_dataset_releases(dag_id=dag_id, entity_id="google_books_traffic")
-                    self.assertEqual(len(dataset_releases), 1)
-                    expected_release = {
-                        "dag_id": dag_id,
-                        "entity_id": "google_books_traffic",
-                        "dag_run_id": release.run_id,
-                        "created": now.to_iso8601_string(),
-                        "modified": now.to_iso8601_string(),
-                        "data_interval_start": "2021-03-31T00:00:00Z",
-                        "data_interval_end": "2021-03-31T12:00:00Z",
-                        "snapshot_date": None,
-                        "partition_date": "2020-02-29T00:00:00Z",
-                        "changefile_start_date": None,
-                        "changefile_end_date": None,
-                        "sequence_start": None,
-                        "sequence_end": None,
-                        "extra": {},
-                    }
-                    self.assertEqual(dataset_releases[0].to_dict(), expected_release)
-
-                    # Test cleanup
-                    workflow_folder_path = release.workflow_folder
-                    ti = env.run_task("process_release.cleanup_workflow", map_index=0)
-                    self.assertEqual(ti.state, State.SUCCESS)
-                    self.assert_cleanup(workflow_folder_path)
+            # Test cleanup
+            self.assert_cleanup(make_workflow_folder(dag_id, dagrun.run_id, make_dir=False))
 
     def test_gb_transform(self):
         """Test sanity check in transform method when transaction date falls outside release month"""
